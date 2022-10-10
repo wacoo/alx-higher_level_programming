9924fb822bf1a346b5c274159800d3e71d658ced
wac <wabaham9@gmail.com>
2022-10-10 05:52:57 -0700

    third commit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as e:
-        print("Exception: {}".format(e),file=sys.stderr)
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]),file=sys.stderr)
         return False
--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_l = [0] * list_length
+    n = 0
+    nl = []
     for idx in range(0, list_length):
         try:
-            new_l[idx] = my_list_1[idx] / my_list_2[idx]
+            n = my_list_1[idx] / my_list_2[idx]
         except ZeroDivisionError:
-            new_l[idx] = 0
+            n = 0
             print("division by 0")
         except TypeError:
-            new_l[idx] = 0
+            n = 0
             print("wrong type")
         except IndexError:
-            new_l[idx] = 0
+            n = 0
             print("out of range")
-    return new_l
+        finally:
+            nl.append(n)
+    return nl
