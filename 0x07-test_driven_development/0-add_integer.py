f7df0330280a27b919f3d7b568512ef9e656d0d8
wac <wabaham9@gmail.com>
2022-10-15 09:24:20 +0000

    first commit

---
+++ 0x07-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A function that adds two integers"""
+
+
+def add_integer(a, b=98):
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    if type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) != int:
+        raise TypeError("b must be an integer")
+    return a + b
