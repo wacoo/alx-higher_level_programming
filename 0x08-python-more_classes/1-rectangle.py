1d24b7091a418115305093101873729972f6a701
wac <wabaham9@gmail.com>
2022-10-17 11:48:38 +0000

    seventh commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
         """ Init a rectangle"""
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        elif not isinstance(width, int):
+        elif not isinstance(height, int):
             raise TypeError("height must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
@@ -48,3 +48,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
