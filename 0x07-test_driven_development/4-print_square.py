144f0d409f8491dce7be4cdfa201f79cfad45200
wac <wabaham9@gmail.com>
2022-10-18 18:26:45 +0000

    first commiy

---
+++ 0x07-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" print square with # """
+
+def print_square(size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print()
