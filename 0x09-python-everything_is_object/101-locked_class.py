da66da4e2a9510b68e1fcfcd3df825aca5994720
wac <wabaham9@gmail.com>
2022-10-18 18:09:45 +0000

    101 commit2

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+
+""" Locked class """
+
+
 class LockedClass:
+    """ Prevents user form instantiating a lockedClass
+        attribute other than first_name """
     __slots__ = ["first_name"]
