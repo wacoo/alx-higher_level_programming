9acbc3f014a40ab7249e2409c176ac2669596a85
wac <wabaham9@gmail.com>
2022-12-21 10:15:49 +0000

    ORM first commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host = "localhost", port = 3306, user = sys.argv[1],
+	 passwd = sys.argv[2], db = sys.argv[3]);
+    db.query(""" SELECT * FROM states ORDER BY states.id ASC""");
+    res = db.store_result();
+    for r in res.fetch_row(maxrows=0):
+        print(r);
