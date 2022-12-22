b9b4bb007e898e5b679bf99048bd77dd860d5a0e
wac <wabaham9@gmail.com>
2022-12-22 18:21:31 +0000

    ORM 10

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -6,19 +6,22 @@ from model_state import Base, State
 from sys import argv
 
 """ insert State object Luosiana to the database"""
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                       .format(argv[1], argv[2], argv[3]))
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-"""Insert a state"""
+    """Insert a state"""
 
-name = "Luosiana"
-st = State(name)
-session.add(st)
-session.flush()
+    name = "Luosiana"
+    st = State(name)
+    session.add(st)
+    session.flush()
 
-""" Show states """
-print(st.id)
-session.commit()
+    """ Show states """
+    print(st.id)
+    session.commit()
+    session.close()
+
