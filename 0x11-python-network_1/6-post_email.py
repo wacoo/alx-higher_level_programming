cbedb096f9e993d2bffb6fbd0792a3b7bc7bb4d2
wac <wabaham9@gmail.com>
2023-01-27 17:51:18 +0000

    0x10/6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python3
-''' Accept url and email and disply email '''
-
-
+#!/usr/bin/python3
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
+"""
+import sys
 import requests
-from sys import argv
 
-data = {'email:' argv[2]}
-r = requests.post(argv[1], data=data)
-print(r.text)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
